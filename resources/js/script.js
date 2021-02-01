bfe415c728cee3000ef0a9815759147c650aee19
gah-code <gilbertharocode@gmail.com>
2021-02-01 00:23:40 -0800

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    $('.js--section-introduction').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+
+    $('.js--scroll-to-form').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-form').offset().top}, 1000);
+    }) 
+
+    /* Navigation scroll */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
+      
+
+     
+
+
+   
+});
+
+ 
